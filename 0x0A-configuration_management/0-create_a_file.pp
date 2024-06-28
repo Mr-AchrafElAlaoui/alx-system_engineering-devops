dd9c42f1778bc88473e177401fbd1170bf74ea3e
Mr.ElAlaoui <hraf23@gmail.com>
2024-06-28 23:29:03 +0100

    Add files.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# use puppet to create a file in tmp directory
+file { '/tmp/school':
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/pup
+# installs flask using puppet
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+}
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
